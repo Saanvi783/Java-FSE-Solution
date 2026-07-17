3f2a100df71ac40d5e07f5def28d520d02f20a94
Saanvi783 <thesaanvidixit@gmail.com>
2026-07-17 11:30:24 +0530

    MyFirstReact_Setup_Solution.js

---
+++ Java FSE/Deepskilling/React/MyFirstReact_Setup_Solution.js
@@ -0,0 +1,9 @@
+// Setup react environment and myfirstreact application
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h1>welcome to the first React app</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
