1106b79797208e577139ceb0658f26131ae9ad4e
Saanvi783 <thesaanvidixit@gmail.com>
2026-07-17 11:30:26 +0530

    NavigationMenuRouter_Solution.js

---
+++ Java FSE/Deepskilling/React/NavigationMenuRouter_Solution.js
@@ -0,0 +1,22 @@
+// Simple Navigation Router using React Router
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function Home() { return <h2>Home Page</h2>; }
+function About() { return <h2>About Page</h2>; }
+
+function NavigationMenuRouter_Solution() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default NavigationMenuRouter_Solution;
