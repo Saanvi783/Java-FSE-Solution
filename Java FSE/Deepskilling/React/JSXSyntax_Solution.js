e1c9aa9fa59d86736d2cec26efac581c9fa47b08
Saanvi783 <thesaanvidixit@gmail.com>
2026-07-17 11:30:27 +0530

    JSXSyntax_Solution.js

---
+++ Java FSE/Deepskilling/React/JSXSyntax_Solution.js
@@ -0,0 +1,13 @@
+// JSX and inline CSS
+import React from 'react';
+
+function JSXSyntax_Solution() {
+  const headingStyle = { color: 'blue', textDecoration: 'underline' };
+  return (
+    <div>
+      <h2 style={headingStyle}>JSX Syntax Demo</h2>
+    </div>
+  );
+}
+
+export default JSXSyntax_Solution;
