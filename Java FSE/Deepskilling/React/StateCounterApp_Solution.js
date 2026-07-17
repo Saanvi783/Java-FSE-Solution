607f44e4a4f75488d763977dc260e74657dfe20c
Saanvi783 <thesaanvidixit@gmail.com>
2026-07-17 11:30:27 +0530

    StateCounterApp_Solution.js

---
+++ Java FSE/Deepskilling/React/StateCounterApp_Solution.js
@@ -0,0 +1,15 @@
+// State Counter App
+import React, { useState } from 'react';
+
+function StateCounterApp_Solution() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default StateCounterApp_Solution;
